0b80b0399ea10d179132c3ecbd853017e23a6961
gmt <generalgmt@yahoo.com>
2014-06-28 16:26:20 +0100

    all working except add

--- shopping.js
+++ shopping.js
@@ -3,6 +3,17 @@ $(document).ready(function(){
 
 $("#form").submit(function(event){
 		event.preventDefault();
+		var curr_item = $("#new_item").val();
+		console.log(curr_item);
+		 $("#item_list").append(curr_item)
+		 // ('<li><input type="checkbox" name="" value="First list"></li>');
+		form.reset();
 	});
 
+	$("li").click(function(){
+		$(this).toggleClass("check_off");	
+	});
+	$("li").dblclick(function(){
+		$(this).remove();	
+	});
 });
