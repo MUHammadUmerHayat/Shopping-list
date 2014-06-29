5593336fb2c42c607c79f40f8ca70d357ce8a141
gmt <generalgmt@yahoo.com>
2014-06-29 06:01:33 +0100

    update from form

--- shopping.js
+++ shopping.js
@@ -1,13 +1,27 @@
 $(document).ready(function(){
-
+$("#err_message").hide();
 
 $("#form").submit(function(event){
 		event.preventDefault();
 		var curr_item = $("#new_item").val();
-		console.log(curr_item);
-		 $("#item_list").append(curr_item)
-		 // ('<li><input type="checkbox" name="" value="First list"></li>');
+		//console.log(curr_item);
+
+		if (curr_item === "")
+		{
+			$("#err_message").show("fast");
+		}
+		else{
+			$("#err_message").hide();
+		var curr_item1 = $(
+        "<li>" +
+        "<input type=\"checkbox\" value=\" \">" +
+         curr_item +
+        "</li>");
+		 $("#item_list").append(curr_item1);
+		 //append('<li><input type="checkbox" name=""> curr_item</li>');
 		form.reset();
+		}
+		
 	});
 
 	$("li").click(function(){
@@ -15,5 +29,8 @@ $("#form").submit(function(event){
 	});
 	$("li").dblclick(function(){
 		$(this).remove();	
+	});
+	$("").click(function(){
+		$('ul').sort();
 	});
 });
